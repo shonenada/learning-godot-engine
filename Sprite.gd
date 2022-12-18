7634148f283a654bbd058ba2a178dd9a72d4f5cd
shonenada <shonenada@gmail.com>
2022-12-19 00:00:29 +0800

    feat: First blood

---
+++ Sprite.gd
@@ -0,0 +1,14 @@
+extends Sprite
+
+var speed = 400
+var angular_speed = PI
+
+func _process(delta):
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+	rotation += angular_speed * delta * direction
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
