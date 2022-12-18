e192a6750a5263edea465849675e7962974e0bf9
shonenada <shonenada@gmail.com>
2022-12-19 00:41:00 +0800

    feat: setup velocity when press up

--- Sprite.gd
+++ Sprite.gd
@@ -5,10 +5,12 @@ var angular_speed = PI
 
 func _process(delta):
 	var direction = 0
+	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_left"):
 		direction = -1
 	if Input.is_action_pressed("ui_right"):
 		direction = 1
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
 	rotation += angular_speed * delta * direction
-	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
