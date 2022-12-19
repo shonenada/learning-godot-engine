22e09ea15bdca64a08deadb21b25060103ebc205
shonenada <shonenada@gmail.com>
2022-12-20 00:43:08 +0800

    feat: add buttion

--- chap1/Sprite.gd
+++ chap1/Sprite.gd
@@ -3,7 +3,7 @@ extends Sprite
 var speed = 400
 var angular_speed = PI
 
-func _process(delta):
+func _process_bakup(delta):
 	var direction = 0
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_left"):
@@ -14,3 +14,13 @@ func _process(delta):
 		velocity = Vector2.UP.rotated(rotation) * speed
 	rotation += angular_speed * delta * direction
 	position += velocity * delta
+	
+	
+func _process(delta):
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
+
+
+func _on_Button_pressed():
+	set_process(not is_processing())
