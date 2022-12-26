a15e5019c11a096578b513536d1121543c788e8e
shonenada <shonenada@gmail.com>
2022-12-26 13:23:40 +0800

    feat: add Main scene

---
+++ demo_2d/Main.gd
@@ -0,0 +1,46 @@
+extends Node
+
+export(PackedScene) var mob_scene
+var score
+
+
+func _ready():
+	randomize()
+	new_game()
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_MobTimer_timeout():
+	var mob = mob_scene.instance()
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	mob.position = mob_spawn_location.position
+	
+	direction += rand_range(-PI/4, PI/4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
