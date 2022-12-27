4ff749ffa34029aed1a5c9da22d4a2e6dc42256a
shonenada <shonenada@gmail.com>
2022-12-27 16:04:11 +0800

    feat: link Main and HUD

--- demo_2d/Main.gd
+++ demo_2d/Main.gd
@@ -12,16 +12,21 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 
 func new_game():
 	score = 0
+	get_tree().call_group("mobs", "queue_free")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_StartTimer_timeout():
