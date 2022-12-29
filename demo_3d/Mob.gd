f8542c934990b4f92ef0b7fca4f5d2d08266c663
shonenada <shonenada@gmail.com>
2022-12-29 15:27:44 +0800

    feat: Add mob scene

---
+++ demo_3d/Mob.gd
@@ -0,0 +1,24 @@
+extends KinematicBody
+
+export var min_speed = 10
+export var max_speed = 18
+
+var velocity = Vector3.ZERO
+
+
+func _physics_process(_delta):
+	# warning-ignore:return_value_discarded
+	move_and_slide(velocity)
+
+
+func initialize(start_position, player_position):
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	rotate_y(rand_range(-PI/4, PI/4))
+	
+	var random_speed = rand_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+func _on_VisibilityNotifier_screen_exited():
+	queue_free()
