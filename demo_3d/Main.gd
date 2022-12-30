0c7bbdbaa9b44ef74180f94b6ccba323ca380c50
shonenada <shonenada@gmail.com>
2022-12-30 11:55:15 +0800

    feat: integrated Mob and Main scene

---
+++ demo_3d/Main.gd
@@ -0,0 +1,18 @@
+extends Node
+
+export(PackedScene) var mob_scene
+
+func _ready():
+	randomize()
+
+
+func _on_MobTimer_timeout():
+	var mob = mob_scene.instance()
+	
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+	mob_spawn_location.unit_offset = randf()
+	
+	var player_position = $Player.transform.origin
+	mob.initialize(mob_spawn_location.translation, player_position)
+	
+	add_child(mob)
