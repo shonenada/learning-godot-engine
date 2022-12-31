515e5e56316e4e580925763ccc959ac92cc5df67
shonenada <shonenada@gmail.com>
2022-12-31 17:00:06 +0800

    feat: Add animation

--- demo_3d/Player.gd
+++ demo_3d/Player.gd
@@ -24,6 +24,9 @@ func _physics_process(delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(translation + direction, Vector3.UP)
+		$AnimationPlayer.playback_speed = 4
+	else:
+		$AnimationPlayer.playback_speed = 1
 
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
@@ -41,6 +44,8 @@ func _physics_process(delta):
 			if Vector3.UP.dot(collision.normal) > 0.1:
 				mob.squash()
 				velocity.y = bounce_impulse
+
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 
 
 func die():
